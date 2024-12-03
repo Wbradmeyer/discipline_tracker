71eb7cd1cb5d27e9aeb1cd35b4d89a30ff5e1b32
W. Brad Meyer <wbradmeyer@gmail.com>
2024-12-03 14:26:54 -0500

    button on display all to nav to create

--- client/src/components/DisplayAll.jsx
+++ client/src/components/DisplayAll.jsx
@@ -1,14 +1,32 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const DisplayAll = () => {
+  const [allGoals, setAllGoals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/goals")
+      .then((res) => {
+        console.log(res);
+        setAllGoals(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
+      <h1>My Goals</h1>
       <div>Goal 1</div>
       <div>Goal 2</div>
       <div>Goal 3</div>
       <div>Goal 4</div>
+      <div>
+        <button>
+          <Link to={"/goals/create"}>+ New Goal</Link>
+        </button>
+      </div>
     </div>
   );
 };
