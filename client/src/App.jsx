6716dff730bd094c2bcca15749dc06efbd97e530
W. Brad Meyer <wbradmeyer@gmail.com>
2024-12-02 22:14:40 -0500

    connect form components to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import DisplayAll from "./components/DisplayAll";
+import GoalForm from "./components/GoalForm";
+import EntryForm from "./components/EntryForm";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<DisplayAll />} />
+          <Route path="/goals/create" element={<GoalForm />} />
+          <Route path="/entries/create" element={<EntryForm />} />
         </Routes>
       </BrowserRouter>
     </div>
