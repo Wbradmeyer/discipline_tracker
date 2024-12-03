58c9d118ecbe1c811ca3a6626b436a139e5fc948
W. Brad Meyer <wbradmeyer@gmail.com>
2024-12-03 13:34:13 -0500

    build out goal form, style

--- client/src/components/GoalForm.jsx
+++ client/src/components/GoalForm.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { React, useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 const GoalForm = () => {
-  return <div>GoalForm</div>;
+  const navigate = useNavigate();
+  const [goal, setGoal] = useState({
+    header: "",
+    intent: "",
+    entries: [],
+  });
+  const [error, setError] = useState({});
+
+  const handleVals = (e) => {
+    setGoal({ ...goal, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/api/goals/create", goal)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.response.data.error.errors.name);
+        setError(err.response.data.error.errors.name);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Add a New Goal</h1>
+      <form onSubmit={onSubmitHandler}>
+        {error ? <p style={{ color: "red" }}>{error.message}</p> : null}
+        <div className="form-items">
+          <label>Header:</label>
+          <input type="text" name="header" onChange={handleVals} />
+        </div>
+        <div className="form-items">
+          <label>Intent Statement (what and target frequency):</label>
+          <input type="text" name="intent" onChange={handleVals} />
+        </div>
+        <button>Create</button>
+      </form>
+    </div>
+  );
 };
 
 export default GoalForm;
