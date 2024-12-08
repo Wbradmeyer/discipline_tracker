11962d7aabfb1f1e2b618f957e4e833d28294d63
W. Brad Meyer <wbradmeyer@gmail.com>
2024-12-07 22:17:43 -0500

    Entry form imports and basic functions

--- client/src/components/EntryForm.jsx
+++ client/src/components/EntryForm.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { React, useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 const EntryForm = () => {
-  return <div>EntryForm</div>;
+  const navigate = useNavigate();
+  const [entry, setEntry] = useState({
+    date: "",
+    dayOfWeek: "",
+    completed: false,
+    description: "",
+    goalId: "",
+  });
+  const [error, setError] = useState({});
+
+  const handleVals = (e) => {
+    setEntry({ ...entry, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/api/entries/create", entry)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.response.data.error.errors.name);
+        setError(err.response.data.error.errors.name);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Add a New Entry</h1>
+    </div>
+  );
 };
 
 export default EntryForm;
