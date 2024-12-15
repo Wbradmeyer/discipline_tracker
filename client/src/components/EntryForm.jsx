4a5459c3d19574bbabfdeec282899f2805d041b1
W. Brad Meyer <wbradmeyer@gmail.com>
2024-12-15 14:39:59 -0500

    change color input tag to have entry.color value

--- client/src/components/EntryForm.jsx
+++ client/src/components/EntryForm.jsx
@@ -108,7 +108,13 @@ const EntryForm = () => {
         </div>
         <div className="form-items">
           <label>Color for Completed</label>
-          <input type="color" name="color" onChange={handleVals} id="color" />
+          <input
+            type="color"
+            name="color"
+            value={entry.color}
+            onChange={handleVals}
+            id="color"
+          />
         </div>
         <div className="form-items">
           <button style={{ margin: "0px 5px" }}>Create</button>
